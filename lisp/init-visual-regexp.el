ac6a2e47781434c41809e41c8afc2909931f8986
Jonathan Hope <jonathan.douglas.hope@gmail.com>
2017-04-04 17:02:51 -0700

    Some regex replace changes:
     - Now using PCRE for matching.
     - Addded Shift + Enter binding for adding a newline in the
     - replacement minibuffer.

--- lisp/init-visual-regexp.el
+++ lisp/init-visual-regexp.el
@@ -1,22 +1,31 @@
 ;; Package configuration for visual-regexp.
 
 (use-package pcre2el
+  :ensure t)
+
+
+(use-package visual-regexp
+  :defer t)
+
+(use-package visual-regexp-steroids
   :ensure t
+  :demand t
+
+  :commands (vr-replace-whole-buffer)
+
+  :bind
+  (:map vr/minibuffer-keymap
+        ("S-<return>" . newline))
 
   :init
   (defun vr-replace-whole-buffer ()
     (interactive)
     (save-excursion
       (beginning-of-buffer)
-      (call-interactively 'vr/replace))))
-
-(use-package visual-regexp-steroids
-  :ensure t
-  :defer t
-
-  :commands (vr-replace-whole-buffer)
+      (call-interactively 'vr/replace)))
 
   :config
-  (setq vr/engine 'pcre2el))
+  (setq vr/engine 'pcre2el)
+  (setq vr/auto-show-help nil))
 
 (provide 'init-visual-regexp)
