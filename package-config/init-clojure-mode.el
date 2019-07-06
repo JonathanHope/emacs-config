60b986312b6044048733a59f37ac43e635f87b7d
Jonathan Hope <jonathan.douglas.hope@gmail.com>
2019-07-05 23:01:49 -0700

    More fontification and symbol replacement for clojure.

--- package-config/init-clojure-mode.el
+++ package-config/init-clojure-mode.el
@@ -14,7 +14,37 @@
         ("C-<tab>" . mainspring-hydra-clojure/body)
         ("<return>". newline-and-indent))
 
+  :init
+
+  (defvar mainspring-clojure-prettify-alist '())
+
+  (add-to-list 'mainspring-clojure-prettify-alist
+               '("<=" . ?≤))
+  (add-to-list 'mainspring-clojure-prettify-alist
+               '(">=" . ?≥))
+  (add-to-list 'mainspring-clojure-prettify-alist
+               '("=>" . ?⇒))
+  (add-to-list 'mainspring-clojure-prettify-alist
+               '("->" . (?- (Br . Bc) ?- (Br . Bc) ?>)))
+  (add-to-list 'mainspring-clojure-prettify-alist
+               '("->>" .  (?\s (Br . Bl) ?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?- (Bc . Br) ?- (Bc . Bc) ?>
+                               (Bc . Bl) ?- (Br . Br) ?>)))
+
+  (eval-after-load 'clojure-mode
+    '(setq clojure--prettify-symbols-alist
+           (append mainspring-clojure-prettify-alist
+                   clojure--prettify-symbols-alist)))
+
   :config
-  (require 'clojure-mode-extra-font-locking))
+  (require 'clojure-mode-extra-font-locking)
+
+  (defvar clojure-midje-vars '("facts" "fact"))
+
+  (font-lock-add-keywords 'clojure-mode
+                          `((,(concat "(\\(?:\.*/\\)?"
+                                      (regexp-opt clojure-midje-vars t)
+                                      "\\>")
+                             1 font-lock-builtin-face))))
 
 (provide 'init-clojure-mode)
