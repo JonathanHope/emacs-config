0af0daf50cc36edad75602ff5a73dfb45b80aabe
Jonathan Hope <jhope@fastmail.com>
2025-05-31 18:47:00 -0700

    feat: subtle copilot

--- package-config/init-copilot.el
+++ package-config/init-copilot.el
@@ -3,9 +3,13 @@
   :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
   :ensure t
 
+  :config
+  (setq copilot-idle-delay nil)
+  
   :bind
   (:map copilot-mode-map
-        ("<backtab>" . copilot-accept-completion))
+        ("<backtab>" . copilot-accept-completion)
+        ("C-:" . copilot-complete))
   
   :hook ((typescript-ts-mode . copilot-mode)
          (tsx-ts-mode . copilot-mode)
